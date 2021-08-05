5938ad1c88ca8d8f7f48b1b9d4c5b544c35f1d1d
Gospel Chukwu <brondchux@gmail.com>
2021-08-05 09:53:38 -0400

    SEnsured readme file automatically gets created

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const genReadmeFile = "./assets/docs/Readme.md";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: "input",
+		name: "title",
+		message: "What is the title of your readme?",
+	},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+	fs.appendFile(fileName, data, (err, done) => {
+		if (err) return console.log("Saving your data failed due to error: ", err);
+		return console.log("Your data was successfuly saved!");
+	});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer
+		.prompt(questions)
+		.then((answers) => {
+			writeToFile(genReadmeFile, answers);
+		})
+		.catch((err) => {});
+}
 
 // Function call to initialize app
 init();
